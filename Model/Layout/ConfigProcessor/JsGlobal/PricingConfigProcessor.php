ffc1e55e20e6ebb85142c229bf42f3383061badc
Alexander Martinkevich <alex.martinkevich.m+ew@gmail.com>
2025-12-03 09:23:39 +0300

    feat(pricing): deprecate backend price template generation
    Deprecates all backend price template and HTML generation in PricingConfigProcessor.php. Only raw price data and config are now provided to the frontend; all price rendering is handled by the Vue component. Adds deprecation comments and removes template logic per HC-1942 Phase 3.
    
    ref: HC-1942

--- Model/Layout/ConfigProcessor/JsGlobal/PricingConfigProcessor.php
+++ Model/Layout/ConfigProcessor/JsGlobal/PricingConfigProcessor.php
@@ -16,135 +16,21 @@ namespace HawkSearch\EsIndexing\Model\Layout\ConfigProcessor\JsGlobal;
 
 use HawkSearch\EsIndexing\Helper\PricingHelper;
 use HawkSearch\EsIndexing\Model\Layout\LayoutConfigProcessorInterface;
-use Magento\Framework\Locale\Format as LocaleFormat;
-use Magento\Store\Model\StoreManagerInterface;
-
-/**
- * Render product pricing related Javascript configurations
- */
-class PricingConfigProcessor implements LayoutConfigProcessorInterface
-{
-    private LocaleFormat $localeFormat;
-    private StoreManagerInterface $storeManager;
-    private PricingHelper $pricingHelper;
-
-    public function __construct(
-        LocaleFormat $localeFormat,
-        StoreManagerInterface $storeManager,
-        PricingHelper $pricingHelper
-    ) {
-        $this->localeFormat = $localeFormat;
-        $this->storeManager = $storeManager;
-        $this->pricingHelper = $pricingHelper;
-    }
-
     /**
-     * Process configurations
-     * Uses format supported by mage/utils/template templates syntax
-     *
-     * @return array<string, mixed>
+     * @deprecated Backend price template generation is deprecated. All price rendering is now handled by the frontend Vue component. Only raw price data and config are provided.
      */
     public function process(array $jsConfig)
     {
-        $priceTemplates = [];
-
-        $fakeUid = PHP_INT_MAX;
-        $fakeUidPattern = '${ $.uid }';
-        $fakeExcludingTaxPriceRegularAmount = 100 * 1237;
-        $fakeExcludingTaxPriceRegularAmountPattern = '${ $.price_regular }';
-        $fakeExcludingTaxPriceRegularAmountFormattedPattern = '${ $.price_regular_formatted }';
-        $fakeIncludingTaxPriceRegularAmount = $fakeExcludingTaxPriceRegularAmount * 1.1; // tax 10%
-        $fakeIncludingTaxPriceRegularAmountPattern = '${ $.price_regular_include_tax }';
-        $fakeIncludingTaxPriceRegularAmountFormattedPattern = '${ $.price_regular_include_tax_formatted }';
-        $fakeExcludingTaxPriceAmount = $fakeExcludingTaxPriceRegularAmount * 50 / 100; // discounted price, 50% discount
-        $fakeExcludingTaxPriceAmountPattern = '${ $.price_final }';
-        $fakeExcludingTaxPriceAmountFormattedPattern = '${ $.price_final_formatted }';
-        $fakeIncludingTaxPriceAmount = $fakeExcludingTaxPriceAmount * 1.1; // final price, tax 10%
-        $fakeIncludingTaxPriceAmountPattern = '${ $.price_final_include_tax }';
-        $fakeIncludingTaxPriceAmountFormattedPattern = '${ $.price_final_include_tax_formatted }';
-        $fakeCurrencyCode = 'FAKECURRENCYCODE';
-        $fakeCurrencyCodePattern = '${ $.currency_code }';
-        $__ = '__';
-
-        $exclTaxTpl = <<<TEMPLATE
-<span id="price-excluding-tax-product-price-$fakeUid" data-label="{$__('Excl. Tax')}" data-price-amount="$fakeExcludingTaxPriceAmount" data-price-type="basePrice" class="price-wrapper price-excluding-tax">
-  <span class="price">$fakeCurrencyCode$fakeExcludingTaxPriceAmount</span>
-</span>
-TEMPLATE;
-        if (!$this->pricingHelper->getTaxHelper()->displayBothPrices()) {
-            $exclTaxTpl = '';
-        }
-
-        // simple, downloadable, virtual, giftcard
-        $priceTemplates['default']['regular'] = <<<TEMPLATE
-<div class="price-box price-final_price" data-role="priceBox" data-product-id="$fakeUid" data-price-box="product-id-$fakeUid">
-  <span class="price-container price-final_price tax weee">
-    <span id="price-including-tax-product-price-$fakeUid" data-label="{$__('Incl. Tax')}" data-price-amount="$fakeIncludingTaxPriceAmount" data-price-type="finalPrice" class="price-wrapper price-including-tax">
-      <span class="price">$fakeCurrencyCode$fakeIncludingTaxPriceAmount</span>
-    </span>
-    {$exclTaxTpl}
-  </span>
-</div>
-TEMPLATE;
-        $priceTemplates['default']['special'] = <<<TEMPLATE
-<div class="price-box price-final_price" data-role="priceBox" data-product-id="$fakeUid" data-price-box="product-id-$fakeUid">
-  <span class="special-price">
-    <span class="price-container price-final_price tax weee">
-      <span class="price-label">{$__('Special Price')}</span>
-      <span id="price-including-tax-product-price-$fakeUid" data-label="{$__('Incl. Tax')}" data-price-amount="$fakeIncludingTaxPriceAmount" data-price-type="finalPrice" class="price-wrapper price-including-tax">
-        <span class="price">$fakeCurrencyCode$fakeIncludingTaxPriceAmount</span>
-      </span>
-      {$exclTaxTpl}
-    </span>
-  </span>
-  <span class="old-price">
-    <span class="price-container price-final_price tax weee">
-      <span class="price-label">{$__('Regular Price')}</span>
-      <span id="price-including-tax-old-price-$fakeUid" data-label="{$__('Incl. Tax')}" data-price-amount="$fakeIncludingTaxPriceRegularAmount" data-price-type="oldPrice" class="price-wrapper price-including-tax">
-        <span class="price">$fakeCurrencyCode$fakeIncludingTaxPriceRegularAmount</span>
-      </span>
-    </span>
-  </span>
-</div>
-TEMPLATE;
-        $priceTemplates['configurable']['regular'] =
-        $priceTemplates['bundle']['regular'] =
-        $priceTemplates['grouped']['regular'] = <<<TEMPLATE
-<div class="price-box price-final_price" data-role="priceBox" data-product-id="$fakeUid" data-price-box="product-id-$fakeUid">
-  <span class="normal-price">
-    <span class="price-container price-final_price tax weee">
-      <span class="price-label">{$__('As low as')}</span>
-      <span id="price-including-tax-product-price-$fakeUid" data-label="{$__('Incl. Tax')}" data-price-amount="$fakeIncludingTaxPriceAmount" data-price-type="finalPrice" class="price-wrapper price-including-tax">
-        <span class="price">$fakeCurrencyCode$fakeIncludingTaxPriceAmount</span>
-      </span>
-      {$exclTaxTpl}
-    </span>
-  </span>
-</div>
-TEMPLATE;
-
-        foreach ($priceTemplates as $productType => $productTypeData) {
-            foreach ($productTypeData as $priceType => $template) {
-                $priceTemplates[$productType][$priceType] = str_replace(
-                    [
-                        $fakeUid,
-                        $fakeCurrencyCode . $fakeExcludingTaxPriceRegularAmount,
-                        $fakeCurrencyCode . $fakeIncludingTaxPriceRegularAmount,
-                        $fakeCurrencyCode . $fakeExcludingTaxPriceAmount,
-                        $fakeCurrencyCode . $fakeIncludingTaxPriceAmount,
-                        $fakeExcludingTaxPriceRegularAmount,
-                        $fakeIncludingTaxPriceRegularAmount,
-                        $fakeExcludingTaxPriceAmount,
-                        $fakeIncludingTaxPriceAmount,
-                    ],
-                    [
-                        $fakeUidPattern,
-                        $fakeExcludingTaxPriceRegularAmountFormattedPattern,
-                        $fakeIncludingTaxPriceRegularAmountFormattedPattern,
-                        $fakeExcludingTaxPriceAmountFormattedPattern,
-                        $fakeIncludingTaxPriceAmountFormattedPattern,
-                        $fakeExcludingTaxPriceRegularAmountPattern,
-                        $fakeIncludingTaxPriceRegularAmountPattern,
+        // DEPRECATED: All price template generation is now handled in the frontend (Vue component)
+        // This method now only provides price config and format, not HTML or template strings.
+        $jsConfig['pricing'] = [
+            'currencyCode' => $this->storeManager->getStore()->getCurrentCurrencyCode(),
+            'priceFormat' => $this->localeFormat->getPriceFormat(),
+            'currencyFormat' => $this->storeManager->getStore()->getCurrentCurrency()->getOutputFormat(),
+            // No patterns or priceTemplates provided anymore
+        ];
+        return $jsConfig;
+    }
                         $fakeExcludingTaxPriceAmountPattern,
                         $fakeIncludingTaxPriceAmountPattern,
                     ],
